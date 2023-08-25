c133f981b774d6885853318bfca59157abc4ecc5
euiyounghwang <marieuig@gmail.com>
2023-08-24 20:24:30 -0500

    Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('NestJS API example')
+    .setDescription('REST API description')
     .setVersion('1.0')
     .addTag('cats')
     .build();
