53b83cfdcd3c2fd033183c0a7f979d68633c94e1
euiyounghwang <marieuig@gmail.com>
2023-08-24 20:50:45 -0500

    Update src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     .setTitle('NestJS API example')
     .setDescription('REST API description')
     .setVersion('1.0')
-    .addTag('cats')
+    // .addTag('Search')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
