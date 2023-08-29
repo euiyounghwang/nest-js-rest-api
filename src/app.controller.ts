0c6ac96ffc1bb95277d1eaee75d1f402fd51a86e
euiyounghwang <marieuig@gmail.com>
2023-08-28 22:50:01 -0500

    API Add

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,51 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Req, HttpCode, HttpStatus, Param, Res, Post, } from '@nestjs/common';
+import { ApiProperty, ApiPropertyOptional, ApiCreatedResponse, ApiUnprocessableEntityResponse, ApiForbiddenResponse, ApiOkResponse } from '@nestjs/swagger';
+import { Response } from 'express';
+import { AppService, StudentResponse } from './app.service';
 
-@Controller()
+@Controller("default")
 export class AppController {
+  @ApiProperty({
+    type: String,
+    description: 'This is a required property',
+  })
+  property: string;
+  @ApiPropertyOptional({
+    type: String,
+    description: 'This is an optional property',
+  })
+  optionalProperty: string;
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  
+  @Get()
+  @HttpCode(200)
+  findAll(@Req() request: Request): string {
+    return 'This action returns all cats';
+  }
+  
+  @ApiOkResponse({
+    description: 'The StudentResponse records',
+    type: StudentResponse,
+    isArray: true
+  })
+  @Get(':id')
+  @HttpCode(200)
+  @ApiCreatedResponse({ description: 'Created Succesfully' })
+  @ApiUnprocessableEntityResponse({ description: 'Bad Request' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  findOne(@Param('id') id: string) {
+    // return {"response" : "This action returns a #${id} cat"};
+    return {"LastName" : id, "FirstName" : id};
+  }
+  
+  @Post()
+  findAllPost(@Res({ passthrough: true }) res: Response) {
+    res.status(HttpStatus.OK);
+    return [];
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+}
+
+export class StudentResponse {
+  @ApiProperty({example: "euiyoung"})
+  FirstName : string;
+  @ApiProperty({example: "hwang"})
+  LastName : string;
 }
